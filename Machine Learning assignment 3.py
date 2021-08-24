a9227a87eea022db69632ffe68e9836563fae9b3
Narender Kumar <narenderbeniwal1234@gmail.com>
2021-08-24 10:43:00 +0530

    Add files via upload

---
+++ Machine Learning assignment 3.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# coding: utf-8
+1.Explain the term machine learning, and how does it work? Explain two machine learning applications in the business world. What are some of the ethical concerns that machine learning applications could raise?
+2. Describe the process of human learning:
+           i. Under the supervision of experts
+
+           ii. With the assistance of experts in an indirect manner
+
+            iii. Self-education
+
+3. Provide a few examples of various types of machine learning.
+
+4. Examine the various forms of machine learning.
+
+5. Can you explain what a well-posed learning problem is? Explain the main characteristics that must be present to identify a learning problem properly.
+
+6. Is machine learning capable of solving all problems? Give a detailed explanation of your answer.
+
+7. What are the various methods and technologies for solving machine learning problems? Any two of them should be defined in detail.
+
+8. Can you explain the various forms of supervised learning? Explain each one with an example application.
+
+9. What is the difference between supervised and unsupervised learning? With a sample application in each region, explain the differences.
+
+10. Describe the machine learning process in depth.
+
+a. Make brief notes on any two of the following:
+
+MATLAB is one of the most widely used programming languages.
+
+        ii. Deep learning applications in healthcare
+
+        iii. Study of the market basket
+
+         iv. Linear regression (simple)
+
+11. Make a comparison between:-
+
+         1. Generalization and abstraction
+
+          2. Learning that is guided and unsupervised
+
+          3. Regression and classification
+1.Explain the term machine learning, and how does it work? Explain two machine learning applications in the business world. What are some of the ethical concerns that machine learning applications could raise?
+Ans: Machine learning is a branch of artificial intelligence (AI) and computer science which focuses on the use of data and algorithms to imitate the way that humans learn, gradually improving its accuracy.
+Application in real world: Youtube and netflix recomendation system, siri, used in ola, uber ete
+
+---Job Loss and Wealth Inequality. ...
+AI is Imperfect — What if it Makes a Mistake? ...
+Should AI Systems Be Allowed to Kill? ...
+Rogue AIs. ...
+Singularity and Keeping Control Over AIs. ...
+How Should We Treat AIs? ...
+AI Bias.----------
+2. Describe the process of human learning:
+ Sol:           i. Under the supervision of experts: Here we learn from an expert.
+
+           ii. With the assistance of experts in an indirect manner
+
+            iii. Self-education: by reading some books, watching vedios, seeing other we can learn many thinks.
+                3. Provide a few examples of various types of machine learning.
+Sol: Supervised learning, unsupervised learning, semi-Supervised learning, Reainforcement learning, online v/s batch learning4. Examine the various forms of machine learning.
+Sol: Supervised Learning.
+Unsupervised Learning.
+Reinforcement Learning. Hybrid Learning Problems.
+Semi-Supervised Learning.
+Self-Supervised Learning.
+Multi-Instance Learning. Statistical Inference.
+Inductive Learning.
+Deductive Inference.5. Can you explain what a well-posed learning problem is? Explain the main characteristics that must be present to identify a learning problem properly.
+Sol: A (machine learning) problem is well-posed if a solution to it exists, if that solution is unique, and if that solution depends on the data / experience but it is not sensitive to (reasonably small) changes in the data / experience.
+
+Identifying Good Problems for ML
+Start with the problem, not the solution. Make sure you aren't treating ML as a hammer for your problems. ...
+Be prepared to have your assumptions challenged. ...
+ML requires a lot of relevant data. ...
+Your features contain predictive power.6. Is machine learning capable of solving all problems? Give a detailed explanation of your answer.
+Ans: No machine learning is not capable of solving all the problems. 
+    ---Creative works like graphics or game designing, Inventions (even the smallest ones) cannot be solved by using Machine Learning.Machine Learning requires data while inventing something requires analytics which currently artificial Intelligence lacks.Even though one can generate arts using GANs but Art is far away for AI to develop.
+
+Also Ethical/Emotional Intelligence problems are far away to be get solved by Artificial Intelligence.---------7. What are the various methods and technologies for solving machine learning problems? Any two of them should be defined in detail.
+
+Ans: Regression.
+Classification.
+Clustering.
+Dimensionality Reduction.
+Ensemble Methods.
+Neural Nets and Deep Learning.
+Transfer Learning.
+Reinforcement Learning.
+
+---technologies--
+TensorFlow. ...
+Keras. ...
+Scikit-learn. ...
+Microsoft Cognitive Toolkit. ...
+Theano. ...
+Caffe. ...
+Torch. ...
+Accord.NET.9. What is the difference between supervised and unsupervised learning? With a sample application in each region, explain the differences.
+Supervised Learning	Unsupervised Learning
+1. Supervised learning algorithms are trained using labeled data.	Unsupervised learning algorithms are trained using unlabeled data.
+2. Supervised learning model takes direct feedback to check if it is predicting correct output or not.	Unsupervised learning model does not take any feedback.
+3. Supervised learning model predicts the output.	Unsupervised learning model finds the hidden patterns in data.
+4. In supervised learning, input data is provided to the model along with the output.	In unsupervised learning, only input data is provided to the model.
+5. The goal of supervised learning is to train the model so that it can predict the output when it is given new data.	The goal of unsupervised learning is to find the hidden patterns and useful insights from the unknown dataset.
+6. Supervised learning needs supervision to train the model.	Unsupervised learning does not need any supervision to train the model.
+7. Supervised learning can be categorized in Classification and Regression problems.	Unsupervised Learning can be classified in Clustering and Associations problems.
+8. Supervised learning can be used for those cases where we know the input as well as corresponding outputs.	Unsupervised learning can be used for those cases where we have only input data and no corresponding output data.
+9. Supervised learning model produces an accurate result.	Unsupervised learning model may give less accurate result as compared to supervised learning.
+10. Supervised learning is not close to true Artificial intelligence as in this, we first train the model for each data, and then only it can predict the correct output.	Unsupervised learning is more close to the true Artificial Intelligence as it learns similarly as a child learns daily routine things by his experiences.
+
+11. It includes various algorithms such as Linear Regression, Logistic Regression, Support Vector Machine, Multi-class Classification, Decision tree, Bayesian Logic, etc.	It includes various algorithms such as Clustering, KNN, and Apriori algorithm
+Q. 10. Describe the machine learning process in depth.
+
+a. Make brief notes on any two of the following:
+
+MATLAB is one of the most widely used programming languages.
+
+        ii. Deep learning applications in healthcare
+
+        iii. Study of the market basket
+
+         iv. Linear regression (simple)
+ii. Deep learning applications in healthcare:---------------
+    1. Identifying Diseases and Diagnosis ·
+    2. Drug Discovery and Manufacturing ·
+    3. Medical Imaging Diagnosis · 
+    4. Personalized Medicine · 
+   
+   iv. Linear regression (simple)
+   Simple linear regression is a statistical method that allows us to summarize and study relationships between two continuous (quantitative) variables.
+
+One variable, denoted x, is regarded as the predictor, explanatory, or independent variable.
+The other variable, denoted y, is regarded as the response, outcome, or dependent variable.
+
+---Height and weight — as height increases, you'd expect weight to increase, but not perfectly.
+Alcohol consumed and blood alcohol content — as alcohol consumption increases, you'd expect one's blood alcohol content to increase, but not perfectly.
+Vital lung capacity and pack-years of smoking — as amount of smoking increases (as quantified by the number of pack-years of smoking), you'd expect lung function (as quantified by vital lung capacity) to decrease, but not perfectly.
+Driving speed and gas mileage — as driving speed increases, you'd expect gas mileage to decrease, but not perfectly.--
+11. Make a comparison between:-
+
+         1. Generalization and abstraction
+
+          2. Learning that is guided and unsupervised
+
+          3. Regression and classification
+          
+          
+ -------Regression:
+Regression is a process of finding the correlations between dependent and independent variables. It helps in predicting the continuous variables such as prediction of Market Trends, prediction of House prices, etc.
+
+The task of the Regression algorithm is to find the mapping function to map the input variable(x) to the continuous output variable(y).
+
+Example: Suppose we want to do weather forecasting, so for this, we will use the Regression algorithm. In weather prediction, the model is trained on the past data, and once the training is completed, it can easily predict the weather for future days.-----------
+
+Regression Algorithm	Classification Algorithm
+In Regression, the output variable must be of continuous nature or real value.	In Classification, the output variable must be a discrete value.
+The task of the regression algorithm is to map the input value (x) with the continuous output variable(y).	The task of the classification algorithm is to map the input value(x) with the discrete output variable(y).
+Regression Algorithms are used with continuous data.	Classification Algorithms are used with discrete data.
+In Regression, we try to find the best fit line, which can predict the output more accurately.	In Classification, we try to find the decision boundary, which can divide the dataset into different classes.
+Regression algorithms can be used to solve the regression problems such as Weather Prediction, House price prediction, etc.	Classification Algorithms can be used to solve classification problems such as Identification of spam emails, Speech Recognition, Identification of cancer cells, etc.
+The regression Algorithm can be further divided into Linear and Non-linear Regression.	The Classification algorithms can be divided into Binary Classifier and Multi-class Classifier.
